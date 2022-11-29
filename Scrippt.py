24fb4a0f4f816c0b371c828ed7e3759fc233f3a2
Opper <opper.d.dor@gmail.com>
2022-11-29 18:43:44 -0300

    Função de mandar o Tutu calar a boca após 3 mensagens

--- Scrippt.py
+++ Scrippt.py
@@ -8,4 +8,20 @@ async def xingamento_de_tutu(evento):
 	if evento.content.lower()=="o tutu é":
 		await evento.message.respond(random.choice(xingamentos))
 
+contador_de_mensagens_do_tutu=0
+
+@Opperado.listen(hikari.MessageCreateEvent)
+async def cala_a_boca(evento):
+	global contador_de_mensagens_do_tutu
+	if evento.message.author.id==624365838610202624:
+		contador_de_mensagens_do_tutu+=1
+	if contador_de_mensagens_do_tutu==3:
+		contador_de_mensagens_do_tutu=0
+		await evento.message.respond("Cala a boca, Tutu!")
+
+
+#async def boas_vindas(evento):
+
+
+
 Opperado.run()
