a323dc032446bf549192b01af9b3223355f2f62a
Opper <opper.d.dor@gmail.com>
2022-11-29 17:49:41 -0300

    Função de xingar o Tutu está opperacional.

--- Scrippt.py
+++ Scrippt.py
@@ -1,5 +1,11 @@
 import hikari
+import random
 
 Opperado = hikari.GatewayBot("MTA0NjkyNjkzODg0NzA2MDAwOA.GmVtNg.mWLRzUMo6NQmGxBpei4zRFaUOUNTJmI6bnyXDY")
+@Opperado.listen(hikari.MessageCreateEvent)
+async def xingamento_de_tutu(evento):
+	xingamentos=["Bobo.","Burro.","Seboso.","Chato."]
+	if evento.content.lower()=="o tutu é":
+		await evento.message.respond(random.choice(xingamentos))
 
 Opperado.run()
