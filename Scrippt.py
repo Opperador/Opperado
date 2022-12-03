2cf3f16e20622e50551c46731c70f83ddf03f1a1
Opper <opper.d.dor@gmail.com>
2022-12-03 00:04:08 -0300

    Novas falas e menos frequencia de cala a bocas
    Adicionei novas falas a função de responder "O tutu é".
    Diminuí a frequencia em que o Opperado manda o Tutu calar a boca e adicionei varições da frase.

--- Scrippt.py
+++ Scrippt.py
@@ -5,7 +5,7 @@ import os
 Opperado = hikari.GatewayBot(os.environ["Token_Opperado"])
 @Opperado.listen(hikari.MessageCreateEvent)
 async def xingamento_de_tutu(evento):
-	xingamentos=["Bobo.","Burro.","Seboso.","Chato."]
+	xingamentos=["Bobo.","Burro.","Seboso.","Chatão, mané.","Boca de sebo."]
 	if evento.content.lower()=="o tutu é":
 		await evento.message.respond(random.choice(xingamentos))
 
@@ -13,11 +13,12 @@ contador_de_mensagens_do_tutu=0
 
 @Opperado.listen(hikari.MessageCreateEvent)
 async def cala_a_boca(evento):
+	calabocamentos=["Quem chamou o Tutu?","Ninguem gosta do Tutu, fica quieto!","Cala a boca, Tutu"]
 	global contador_de_mensagens_do_tutu
 	if evento.message.author.id==624365838610202624:
 		contador_de_mensagens_do_tutu+=1
-	if contador_de_mensagens_do_tutu==3:
+	if contador_de_mensagens_do_tutu==30:
 		contador_de_mensagens_do_tutu=0
-		await evento.message.respond("Cala a boca, Tutu!")
+		await evento.message.respond(random.choice(calabocamentos))
 
 Opperado.run()
