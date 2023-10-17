cb09e255f99d9be555de74f4ec7ca961b7cd712a
JMV <jmv-11787@outlook.com>
2023-10-16 23:05:55 -0300

    Sincronia com o banco de dados
    A contagem de mensagens do tutu é sincronizada com o banco de dados.

--- opperado.py
+++ opperado.py
@@ -10,7 +10,9 @@ cliente_banco_de_dados = pymongo.MongoClient(os.environ["LINHA_DE_CONEXAO_DO_MON
 
 xingamentos = cliente_banco_de_dados["opperado"]["opperado"].find_one()["xingamentos"]
 calabocamentos = cliente_banco_de_dados["opperado"]["opperado"].find_one()["calabocamentos"]
-contador_de_mensagens_do_tutu = 0
+contador_de_mensagens_do_tutu = cliente_banco_de_dados["opperado"]["opperado"].find_one()[
+	"contador_de_mensagens_do_tutu"
+]
 
 
 @opperado.listen(hikari.MessageCreateEvent)
@@ -20,15 +22,24 @@ async def xingamento_de_tutu(evento):
 		await evento.message.respond(random.choice(xingamentos))
 
 
+def atualizar_a_contagem_do_tutu():
+	cliente_banco_de_dados["opperado"]["opperado"].update_one(
+		{}, {"$set": {"contador_de_mensagens_do_tutu": contador_de_mensagens_do_tutu}}
+	)
+
+
 @opperado.listen(hikari.MessageCreateEvent)
 async def cala_a_boca(evento):
 	global calabocamentos
 	global contador_de_mensagens_do_tutu
 	if evento.message.author.id == os.environ.get("ID_DO_TUTU", 624365838610202624):
 		contador_de_mensagens_do_tutu += 1
+		atualizar_a_contagem_do_tutu()
 	if contador_de_mensagens_do_tutu == 30:
 		contador_de_mensagens_do_tutu = 0
+		atualizar_a_contagem_do_tutu()
 		await evento.message.respond(random.choice(calabocamentos))
 
 
 opperado.run()
+cliente_banco_de_dados.close()
