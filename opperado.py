d01d4542bd71c09ea0d257eb03a24c5b27899ef2
JMV <jmv-11787@outlook.com>
2023-10-18 16:18:29 -0300

    Dicas de tipo

--- opperado.py
+++ opperado.py
@@ -1,3 +1,4 @@
+from typing import Final
 import random
 import os
 
@@ -5,31 +6,33 @@ import hikari
 import pymongo
 
 
-opperado = hikari.GatewayBot(os.environ["FICHA_DE_OPPERADO"])
-cliente_banco_de_dados = pymongo.MongoClient(os.environ["LINHA_DE_CONEXAO_DO_MONGODB_DE_OPPERADO"])
+opperado: Final[hikari.GatewayBot] = hikari.GatewayBot(os.environ["FICHA_DE_OPPERADO"])
+cliente_banco_de_dados: Final[pymongo.MongoClient] = pymongo.MongoClient(
+	os.environ["LINHA_DE_CONEXAO_DO_MONGODB_DE_OPPERADO"]
+)
 
-xingamentos = cliente_banco_de_dados["opperado"]["opperado"].find_one()["xingamentos"]
-calabocamentos = cliente_banco_de_dados["opperado"]["opperado"].find_one()["calabocamentos"]
-contador_de_mensagens_do_tutu = cliente_banco_de_dados["opperado"]["opperado"].find_one()[
+xingamentos: list[str] = cliente_banco_de_dados["opperado"]["opperado"].find_one()["xingamentos"]
+calabocamentos: list[str] = cliente_banco_de_dados["opperado"]["opperado"].find_one()["calabocamentos"]
+contador_de_mensagens_do_tutu: int = cliente_banco_de_dados["opperado"]["opperado"].find_one()[
 	"contador_de_mensagens_do_tutu"
 ]
 
 
-@opperado.listen(hikari.MessageCreateEvent)
-async def xingamento_de_tutu(evento):
+@opperado.listen()
+async def xingamento_de_tutu(evento: hikari.MessageCreateEvent) -> None:
 	global xingamentos
 	if evento.content.lower() == "o tutu é":
 		await evento.message.respond(random.choice(xingamentos))
 
 
-def atualizar_a_contagem_do_tutu():
+def atualizar_a_contagem_do_tutu() -> None:
 	cliente_banco_de_dados["opperado"]["opperado"].update_one(
 		{}, {"$set": {"contador_de_mensagens_do_tutu": contador_de_mensagens_do_tutu}}
 	)
 
 
-@opperado.listen(hikari.MessageCreateEvent)
-async def cala_a_boca(evento):
+@opperado.listen()
+async def cala_a_boca(evento: hikari.MessageCreateEvent) -> None:
 	global calabocamentos
 	global contador_de_mensagens_do_tutu
 	if evento.message.author.id == int(os.environ.get("ID_DO_TUTU", 624365838610202624)):
